0d8e138a7e17df26b62e04237b62c7af2b503b2e
Zack <115175938+ZackWilde27@users.noreply.github.com>
2025-06-07 16:19:34 -0600

    Fixed the fixer
    When fetching the PNG version of a DDS, it now loads a new image instead of replacing the current, so now the different track materials should use separate textures as intended.
    
    Added more checks to the Track UV Fix to make sure it should be applied. There's a new option that automatically undo's the bugged Track UV Fix

--- fo2materialfixer.py
+++ fo2materialfixer.py
@@ -15,25 +15,27 @@ class DDSToPNG(bpy.types.Operator):
     folderPath: bpy.props.StringProperty(name="Texture Folder Path")
 
     def execute(self, context):
+        if self.folderPath[-1] not in "/\\":
+            self.folderPath += "\\" if "\\" in self.folderPath else "/"
+        
         for object in bpy.data.objects:
             if not object.data: continue
-        
-            image = object.data.materials[0].node_tree.nodes[3].image
-            path = image.filepath
-            if self.folderPath[-1] not in "/\\":
-                self.folderPath += "\\" if "\\" in self.folderPath else "/"
+
+            path = object.data.materials[0].node_tree.nodes[3].image.filepath
+            
             if "." in path:
                 if path[path.rindex(".") + 1:] == "dds":
-                    filename = path[path.rindex("\\" if "\\" in path else "//") + 1:path.rindex(".")]
+                    
+                    filename = path[path.rindex("\\" if "\\" in path else "/") + 1:]
 
-                    matName = object.data.materials[0].name.lower()
-                    if matName.startswith("sdm_") or matName.startswith("dm_"):
-                        filename = matName
-                        if "." in filename:
-                            filename = filename[:filename.index(".")]
+                    if IsRoadMaterial(object.data.materials[0]):
+                        filename = object.data.materials[0].name.lower()
+                    
+                    # Clearing out both the extension from the filename, and any .00[n]'s that might be on the material name
+                    if "." in filename:
+                        filename = filename[:filename.index(".")]
 
-                    image.filepath = self.folderPath + filename + ".png"
-                    image.reload()
+                    object.data.materials[0].node_tree.nodes[3].image = bpy.data.images.load(self.folderPath + filename + ".png", check_existing=True)
                  
         return { 'FINISHED' }
     
@@ -55,24 +57,37 @@ class AlphaFix(bpy.types.Operator):
                 tree.links.new(tree.nodes[0].inputs[4], tree.nodes[3].outputs[1])
                  
         return { 'FINISHED' }
+    
+def IsRoadMaterial(material):
+    matName = material.name.lower()
+    return matName.startswith("sdm_") or matName.startswith("dm_")
 
 
 class LevelUVFix(bpy.types.Operator):
     """Finds all track geometry and fixes the UV map"""
     bl_idname = "fo2materialfixer.leveluvfix"
     bl_label = "Fix Track UVs"
+    
+    fixOldBug: bpy.props.BoolProperty(name="Undo UV fix on non-track")
 
     def execute(self, context):
         for object in bpy.data.objects:
             if not object.data: continue
         
+            matName = object.data.materials[0].name
             if len(object.data.uv_layers) > 1:
-                object.data.uv_layers[1].active_render = True
-                 
+                if IsRoadMaterial(object.data.materials[0]):
+                    object.data.uv_layers[1].active_render = True
+
+                # Automates fixing the bug from the last version
+                elif self.fixOldBug and object.data.uv_layers[1].active_render:
+                    object.data.uv_layers[0].active_render = True
+
         return { 'FINISHED' }
-
-
-    
+        
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self)
 
 
 class CustomMenu(bpy.types.Menu):
